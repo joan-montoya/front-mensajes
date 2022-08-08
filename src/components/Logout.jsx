7d1e98dca1cd6f75afd04b90624787c302672433
IvanLF <ivaanlopez602@gmail.com>
2022-08-08 02:40:15 -0500

    style: style is integrated.

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -22,3 +22,21 @@ export default function Logout() {
     </Button>
   );
 }
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  background-color: #9a86f3;
+  border: none;
+  cursor: pointer;
+  position: relative;
+  left: 11rem;
+  top: -2.2rem;
+  svg {
+    font-size: 1.3rem;
+    color: #ebe7ff;
+  }
+`;
